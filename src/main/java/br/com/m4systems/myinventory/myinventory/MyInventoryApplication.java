ed4dc5e0aa850f42b8fc7d0e306f2dd8d148e8c1
Marcelo Dias <m4rcelodias@live.com>
2023-09-26 20:23:54 -0300

    Iniciando o projeto. Estrutura do projeto criado.

---
+++ src/main/java/br/com/m4systems/myinventory/myinventory/MyInventoryApplication.java
@@ -0,0 +1,13 @@
+package br.com.m4systems.myinventory.myinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyInventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/m4systems/myinventory/myinventory/MyInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.m4systems.myinventory.myinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
