8096f44c6eb0366e3263afcd61a830a8fcad09c2
Marcelo Dias <m4rcelodias@live.com>
2023-09-27 18:10:16 -0300

    Alterado o INDEX Criado o LOGIN Criado o Styles.css SecurityConfig.java esta vazio por enquanto.

--- src/main/java/br/com/m4systems/myinventory/config/SecurityConfig.java
+++ src/main/java/br/com/m4systems/myinventory/config/SecurityConfig.java
@@ -1,14 +1,10 @@
 package br.com.m4systems.myinventory.config;
 
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.ObjectPostProcessor;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfiguration {
+public class SecurityConfig {
 
 }
